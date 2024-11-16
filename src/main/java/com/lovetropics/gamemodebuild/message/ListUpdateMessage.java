20a86530480493ecebb6f65dc6a68327054fbf34
Gegy <gegy.dev@gmail.com>
2024-11-16 03:10:37 +0100

    Fix: don't fail to encode packet when clearing list

--- src/main/java/com/lovetropics/gamemodebuild/message/ListUpdateMessage.java
+++ src/main/java/com/lovetropics/gamemodebuild/message/ListUpdateMessage.java
@@ -8,21 +8,20 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-public record ListUpdateMessage(Operation operation, boolean whitelist, String name, String entry) implements CustomPacketPayload {
+import java.util.Optional;
+
+public record ListUpdateMessage(Operation operation, boolean whitelist, String name, Optional<String> entry) implements CustomPacketPayload {
 	public static final Type<ListUpdateMessage> TYPE = new Type<>(GamemodeBuild.rl("update_list"));
 	public static final StreamCodec<RegistryFriendlyByteBuf, ListUpdateMessage> CODEC = StreamCodec.composite(
-			ByteBufCodecs.idMapper(i -> Operation.values()[i], Operation::ordinal),
-			ListUpdateMessage::operation,
-			ByteBufCodecs.BOOL,
-			ListUpdateMessage::whitelist,
-			ByteBufCodecs.STRING_UTF8,
-			ListUpdateMessage::name,
-			ByteBufCodecs.STRING_UTF8,
-			ListUpdateMessage::entry,
+			ByteBufCodecs.idMapper(i -> Operation.values()[i], Operation::ordinal), ListUpdateMessage::operation,
+			ByteBufCodecs.BOOL, ListUpdateMessage::whitelist,
+			ByteBufCodecs.STRING_UTF8, ListUpdateMessage::name,
+			ByteBufCodecs.STRING_UTF8.apply(ByteBufCodecs::optional), ListUpdateMessage::entry,
 			ListUpdateMessage::new
 	);
 
 	public void handle(IPayloadContext context) {
+		String entry = this.entry.orElse("");
 		switch (operation) {
 			case ADD -> {
 				if (whitelist) {
