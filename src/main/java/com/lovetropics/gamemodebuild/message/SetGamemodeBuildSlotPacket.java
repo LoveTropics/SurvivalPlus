112eeb2b71a9b13e7efdd0d0d662d41f4b3398f0
Gegy <gegy.dev@gmail.com>
2024-11-16 03:06:27 +0100

    Simplify SetGamemodeBuildSlotPacket a bit

--- src/main/java/com/lovetropics/gamemodebuild/message/SetGamemodeBuildSlotPacket.java
+++ src/main/java/com/lovetropics/gamemodebuild/message/SetGamemodeBuildSlotPacket.java
@@ -2,112 +2,101 @@ package com.lovetropics.gamemodebuild.message;
 
 import com.lovetropics.gamemodebuild.GBConfigs;
 import com.lovetropics.gamemodebuild.GamemodeBuild;
-import com.lovetropics.gamemodebuild.container.BuildContainer;
 import com.lovetropics.gamemodebuild.container.GBStackMarker;
 import com.lovetropics.gamemodebuild.state.GBPlayerStore;
 import com.lovetropics.gamemodebuild.state.GBServerState;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.network.protocol.PacketUtils;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.network.protocol.game.ServerboundSetCreativeModeSlotPacket;
+import net.minecraft.network.protocol.game.ClientboundSetCarriedItemPacket;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.CustomData;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-import java.util.List;
 import java.util.function.Predicate;
 
-public record SetGamemodeBuildSlotPacket(short slotNum, BlockPos blockPos)  implements CustomPacketPayload {
-    public static final Type<SetGamemodeBuildSlotPacket> TYPE = new Type<>(GamemodeBuild.rl("set_slot"));
-    public static final StreamCodec<RegistryFriendlyByteBuf, SetGamemodeBuildSlotPacket> CODEC;
-    static {
-        CODEC = StreamCodec.composite(ByteBufCodecs.SHORT, SetGamemodeBuildSlotPacket::slotNum, BlockPos.STREAM_CODEC, SetGamemodeBuildSlotPacket::blockPos, SetGamemodeBuildSlotPacket::new);
-    }
+// TODO: Won't need a custom packet in 1.21.4+ - it's server-authoritative
+public record SetGamemodeBuildSlotPacket(BlockPos blockPos) implements CustomPacketPayload {
+    public static final Type<SetGamemodeBuildSlotPacket> TYPE = new Type<>(GamemodeBuild.rl("pick_block"));
 
-    public SetGamemodeBuildSlotPacket(short slotNum, BlockPos blockPos) {
-        this.slotNum = slotNum;
-        this.blockPos = blockPos;
-    }
+    public static final StreamCodec<RegistryFriendlyByteBuf, SetGamemodeBuildSlotPacket> CODEC = StreamCodec.composite(
+            BlockPos.STREAM_CODEC, SetGamemodeBuildSlotPacket::blockPos,
+            SetGamemodeBuildSlotPacket::new
+    );
 
     public static void handle(SetGamemodeBuildSlotPacket packet, IPayloadContext ctx) {
-        var player = ctx.player();
+        if (!(ctx.player() instanceof ServerPlayer player) || !GBServerState.isActiveFor(player)) {
+            return;
+        }
 
-        if (player instanceof ServerPlayer serverPlayer) {
-            ctx.enqueueWork(() -> {
-                if(!player.canInteractWithBlock(packet.blockPos, ServerPlayer.INTERACTION_DISTANCE_VERIFICATION_BUFFER)) {
-                    return;
-                }
+        BlockPos blockPos = packet.blockPos;
+        if (!player.canInteractWithBlock(blockPos, ServerPlayer.INTERACTION_DISTANCE_VERIFICATION_BUFFER)) {
+            return;
+        }
 
-                if (GBServerState.isActiveFor(serverPlayer)) {
-                    ItemStack itemstack;
+        BlockState blockState = player.level().getBlockState(blockPos);
 
-                    FeatureFlagSet featureFlags = player.level().enabledFeatures();
-                    RegistryAccess registryAccess = player.level().registryAccess();
-                    Predicate<ItemStack> itemStacks = GBConfigs.SERVER.getFilter(GBPlayerStore.getList(player)).getStackPredicate(featureFlags, registryAccess);
+        // Just fake it, it's not important
+        BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(blockPos), Direction.UP, blockPos, false);
+        ItemStack itemStack = blockState.getCloneItemStack(hitResult, player.level(), blockPos, player);
+        if (itemStack.isEmpty()) {
+            return;
+        }
 
-                    BlockState blockstate = player.level().getBlockState(packet.blockPos);
-                    if (blockstate.isAir()) {
-                        return;
-                    }
+        FeatureFlagSet enabledFeatures = player.level().enabledFeatures();
+        RegistryAccess registryAccess = player.level().registryAccess();
+        Predicate<ItemStack> predicate = GBConfigs.SERVER.getFilter(GBPlayerStore.getList(player)).getStackPredicate(enabledFeatures, registryAccess);
+        if (!predicate.test(itemStack)) {
+            return;
+        }
 
-                    itemstack = new ItemStack(blockstate.getBlock());
+        GBStackMarker.mark(itemStack);
+        itemStack.setCount(itemStack.getMaxStackSize());
 
-                    GBStackMarker.mark(itemstack);
+        pickItem(player, itemStack);
+    }
 
-                    if (itemstack.isEmpty()) {
-                        return;
-                    }
+    private static void pickItem(ServerPlayer player, ItemStack itemStack) {
+        Inventory inventory = player.getInventory();
+        int slotWithItem = inventory.findSlotMatchingItem(itemStack);
+        if (slotWithItem == Inventory.NOT_FOUND_INDEX) {
+            pickFreshItem(inventory, itemStack);
+        } else {
+            pickExistingItem(inventory, slotWithItem);
+        }
+        player.connection.send(new ClientboundSetCarriedItemPacket(inventory.selected));
+        player.inventoryMenu.broadcastChanges();
+    }
 
-                    if(!itemStacks.test(itemstack)) {
-                        player.inventoryMenu.setRemoteSlot(packet.slotNum, itemstack);
-                        player.inventoryMenu.broadcastChanges();
-                        return;
-                    }
-
-                    if (!itemstack.isItemEnabled(player.level().enabledFeatures())) {
-                        return;
-                    }
-
-                    CustomData customdata = itemstack.getOrDefault(DataComponents.BLOCK_ENTITY_DATA, CustomData.EMPTY);
-                    if (customdata.contains("x") && customdata.contains("y") && customdata.contains("z")) {
-                        BlockPos blockpos = BlockEntity.getPosFromTag(customdata.getUnsafe());
-                        if (player.level().isLoaded(blockpos)) {
-                            BlockEntity blockentity = player.level().getBlockEntity(blockpos);
-                            if (blockentity != null) {
-                                blockentity.saveToItem(itemstack, player.level().registryAccess());
-                            }
-                        }
-                    }
-
-                    boolean flag1 = packet.slotNum() >= 1 && packet.slotNum() <= 45;
-                    boolean flag2 = itemstack.isEmpty() || itemstack.getCount() <= itemstack.getMaxStackSize();
-                    if (flag1 && flag2) {
-                        player.inventoryMenu.getSlot(packet.slotNum()).setByPlayer(itemstack);
-                        player.inventoryMenu.broadcastChanges();
-                    }
-                }
-            });
+    private static void pickExistingItem(Inventory inventory, int slot) {
+        if (Inventory.isHotbarSlot(slot)) {
+            inventory.selected = slot;
+        } else {
+            inventory.pickSlot(slot);
         }
     }
 
-    @Override
-    public Type<? extends CustomPacketPayload> type() {
-        return TYPE;
+    private static void pickFreshItem(Inventory inventory, ItemStack itemStack) {
+        inventory.selected = inventory.getSuitableHotbarSlot();
+        if (!inventory.getItem(inventory.selected).isEmpty()) {
+            int freeSlot = inventory.getFreeSlot();
+            if (freeSlot != Inventory.NOT_FOUND_INDEX) {
+                inventory.setItem(freeSlot, inventory.getItem(inventory.selected));
+            }
+        }
+        inventory.setItem(inventory.selected, itemStack);
     }
 
-
-    public short slotNum() {
-        return this.slotNum;
+    @Override
+    public Type<SetGamemodeBuildSlotPacket> type() {
+        return TYPE;
     }
 }
